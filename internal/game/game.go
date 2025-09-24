ae26a957929157de7e021a3a23a70a237bde4295
Agostino Imbimbo parra <agoimbimboparra@gmail.com>
2025-09-24 18:04:50 -0400

    Added code to pass tests

--- internal/game/game.go
+++ internal/game/game.go
@@ -1,5 +1,7 @@
 package game
 
+import "fmt"
+
 type Player struct {
 	Name string
 }
@@ -18,6 +20,15 @@ func (g *Game) AddPlayer(name string) {
 	g.Players = append(g.Players, Player{Name: name})
 }
 
+var ErrNotEnoughPlayers = fmt.Errorf("not enough players to start the game")
+var ErrTooManyPlayers = fmt.Errorf("too many players to start the game")
+
 func (g *Game) StartGame() error {
+	if len(g.Players) < 5 {
+		return ErrNotEnoughPlayers
+	} else if len(g.Players) >= 6 {
+		return ErrTooManyPlayers
+	}
+	// Additional game start logic would go here
 	return nil
 }
