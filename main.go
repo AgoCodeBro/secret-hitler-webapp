dcdc89a82c7ca720b66193e86bb44753095812e2
Agostino Imbimbo parra <agoimbimboparra@gmail.com>
2025-09-24 17:41:43 -0400

    Addressed possible security issue

--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
 	svr := &http.Server{
-		Addr:    ":" + port,
-		Handler: mux,
+		Addr:              ":" + port,
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
 	}
 
 	log.Printf("Serving on port %v", port)
